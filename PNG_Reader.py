e6b31626e3e92023332a2bc08cf19088f5d46b77
LucasT-UC <lucastb@uc.cl>
2022-03-22 11:42:00 -0300

    avance

--- PNG_Reader.py
+++ PNG_Reader.py
@@ -1,4 +1,4 @@
-import imageio
+import imageio, os
 import numpy as np
 from tkinter import filedialog, Tk
 
@@ -10,27 +10,27 @@ def main():
     print(op)
     print(type(op))
     if op == "1":
-        root = Tk()
-        root.withdraw()
-        root.lift()
-
-        file_path = filedialog.askopenfilename()
-        im = imageio.imread(file_path)
-        dimensions = im.size//256
-        pixels = np.zeros((16, 16, 3))
-        if dimensions == 3 or dimensions == 4:
-            for y in range(len(im)):
-                for x in range(len(im[y])):
-                    pixels[y][x][0] = im[x][y][0]
-                    pixels[y][x][1] = im[x][y][1]
-                    pixels[y][x][2] = im[x][y][2]
-            print(pixels)
-        else:
-            print(f"Can't read a png with {dimensions} data values")
-
+        directory = "C:\Users\ltapi\Desktop\Universidad\Fun\LED_PROJECTS\input"
+        for f in os.listdir(directory):
+            f = os.path.join(directory, f)
+            send_image(f)
     elif op == "0":
         raise SystemExit
 
+
+def send_image(f):
+    im = imageio.imread(f)
+    dimensions = im.size//256
+    pixels = np.zeros((16, 16, 3))
+    if dimensions == 3 or dimensions == 4:
+        for y in range(len(im)):
+            for x in range(len(im[y])):
+                pixels[y][x][0] = im[x][y][0]
+                pixels[y][x][1] = im[x][y][1]
+                pixels[y][x][2] = im[x][y][2]
+        print(pixels)
+    else:
+        print(f"Can't read a png with {dimensions} data values")
+    
 if __name__ == "__main__":
     main()
-
